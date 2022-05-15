93fb860e89dad9f745b2ae8ee71ea946bbd6c8cd
= <macd.collin@gmail.com>
2022-05-14 23:14:59 -0500

    Create main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import tmdbLogo from './images/tmdbLogo.svg'
 import './App.css';
+import { useState } from "react";
+const axios = require('axios');
 
 function App() {
+  let [searchText, setSearchText] = useState("");
+
+  let handleInputChanges = (event) => {
+    setSearchText(event.currentTarget.value);
+  };
+
+  function logMovies(movie) {
+    console.log(movie.original_title);
+  }
+
+  function submitSearch () {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=***REMOVED***&query=' + searchText;
+    
+    console.log(url);
+
+    axios
+      .get(url)
+      .then(res => {
+          res.data.results.forEach(logMovies)
+      })
+      .catch(error => {
+          console.error(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Search
+      </div>
+      <div>
+        <input
+          onChange={handleInputChanges}
+        ></input>
+        <button onClick={submitSearch}>Submit</button>
+      </div>
+      <div>
+        Powered by
+      </div>
+      <div> 
+        <img src={tmdbLogo} className="tmdbLogo"></img>
+      </div>
     </div>
   );
 }
