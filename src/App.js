861dd4e629350387d05df62d487abedaacae95b7
Collin MacDonald <macd.collin@gmail.com>
2022-05-17 12:32:31 -0500

    move api calls into app.js

--- src/App.js
+++ src/App.js
@@ -56,3 +56,53 @@ function App() {
 }
 
 export default App;
+
+
+// var express = require('express');
+// const axios = require('axios');
+// const { MongoClient } = require("mongodb");
+// var app = express();
+
+// const uri = 'mongodb+srv://collinmacd:PASSWORD@cluster0.ppihs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const client = new MongoClient(uri);
+
+// app.get('/', function (req, res) {
+//     res.send('healthcheck');
+//  })
+
+// app.get('/getRelatedMovies', function (req, res) {
+//     res.send(req.query.id);
+
+//     // Get all the relationships saved in MongoDB for this movie
+//     async function run() {
+//         try {
+//           await client.connect();
+//           const database = client.db('film_relationships');
+//           const relationships = database.collection('relationships');
+//           const query = { first_id: req.query.id };
+//           const relationship = await relationships.findOne(query);
+//           const query2 = { second_id: req.query.id };
+//           const relationship2 = await relationships.findOne(query2);
+
+//           if (!!relationship) {
+//               console.log("this film is required or suggested for another film");
+//           }
+//           if (!!relationship2) {
+//               console.log("this film has suggestions and requirements");
+//           }
+//           if (!relationship && !relationship2) {
+//               console.log("nothing found for this film");
+//           }
+//         } finally {
+//           // Ensures that the client will close when you finish/error
+//           await client.close();
+//         }
+//       }
+//     run().catch(console.dir);
+// })
+
+// app.listen(8081, function () {
+//    var host = '127.0.0.1';
+//    var port = 8081;
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
